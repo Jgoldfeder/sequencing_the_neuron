31a654ebd6f50bf97548fcebc1b1767d226ca13d
Yau-Meng Wong <yw3809@columbia.edu>
2024-10-27 12:10:08 -0400

    removed first distillation code

--- distillation/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .imagenet import imagenet_model_dict
-from .cifar import cifar_model_dict
--- distillation/models/imagenet/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
-
-
-imagenet_model_dict = {
-    "ResNet18": resnet18,
-    "ResNet34": resnet34,
-    "ResNet50": resnet50,
-    "ResNet101": resnet101,
-}
